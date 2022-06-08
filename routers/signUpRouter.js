42917884fd967cbb3221f433ddef336a067ef002
Matheus Moura <matheuslnmoura@gmail.com>
2022-06-08 06:04:31 -0300

    If applied, this commit will implement the 'POST/signup' Endpoint with all requirements and validations met.

--- routers/signUpRouter.js
+++ routers/signUpRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
+import validateSignUpData from '../middlewares/signUpValidation.js';
 import { signUp } from '../controllers/signUpController.js';
 
 const signUpRouter = Router();
 
-signUpRouter.post('/signup', signUp);
+signUpRouter.post('/signup', validateSignUpData, signUp);
 
 export default signUpRouter;
