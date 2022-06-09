ba5523b4611263cfbea73e09d2745e103d0467f2
Matheus Moura <matheuslnmoura@gmail.com>
2022-06-09 14:13:35 -0300

    If applied, this commit will implement the 'GET /urls/open/:shortUrl' Endpoint. This endpoint will need to be tested on deploy environment to make sure that all requirements and validations are met

--- controllers/urlsController.js
+++ controllers/urlsController.js
@@ -2,7 +2,7 @@ import chalk from 'chalk';
 
 import connection from '../db.js';
 
-export default async function getUrl(req,res) {
+export async function getUrl(req,res) {
 	try {
 		const urlIdObj = req.params;
 		const {id} = urlIdObj;
@@ -22,4 +22,39 @@ export default async function getUrl(req,res) {
 		console.log(chalk.bold.red(error));
 		return res.sendStatus(500);
 	}
+}
+
+export async function openUrl(req, res) {
+	try {
+		const shortUrlObj = req.params;
+		const {shortUrl} = shortUrlObj;
+
+		const dataBaseUrl = (await connection.query(`
+		    SELECT urls.url, urls."visitCount"
+		    FROM urls
+		    WHERE urls."shortUrl" = ($1)
+		`, [shortUrl])).rows[0];
+
+		if(!dataBaseUrl) {
+			return res.status(404).send('Url not found :(');
+		}
+
+		const {url: redirectUrl, visitCount} = dataBaseUrl; 
+		updateVisitCount(shortUrl, visitCount);
+
+		return res.redirect(200, redirectUrl);
+
+	} catch (error) {
+		console.log(chalk.bold.red(error));
+		return res.sendStatus(500);
+	}
+}
+
+async function updateVisitCount(shortUrl, visitCount) {
+	visitCount++;
+	await connection.query(`
+	    UPDATE urls SET "visitCount" = ($1)
+        WHERE "shortUrl" = ($2)
+	`, [visitCount, shortUrl]);
+
 }
--- routers/urlRouter.js
+++ routers/urlRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
-import getUrl from '../controllers/urlsController.js';
+import {getUrl, openUrl} from '../controllers/urlsController.js';
 
 const urlRouter = Router();
 
 urlRouter.get('/urls/:id', getUrl);
+urlRouter.get('/urls/open/:shortUrl', openUrl);
 
 export default urlRouter;
