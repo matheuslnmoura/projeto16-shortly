b290c612bc1539e548cf9dc9792e992db559fdb7
Matheus Moura <matheuslnmoura@gmail.com>
2022-06-09 23:37:19 -0300

    If applied, this commit will implement the 'GET /ranking' Endpoint with all requirements met

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -37,3 +37,21 @@ export async function getUser(req,res) {
 		return res.sendStatus(500);
 	}
 }
+
+export async function getUsersRank(req,res) {
+	try {
+		const dataBaseRank = (await connection.query(`
+            SELECT users.id, users.name, COUNT(urls.id), COALESCE(SUM(urls."visitCount"), 0) AS "visitCount"
+            FROM urls
+            RIGHT JOIN users ON urls."userId" = users.id
+            GROUP BY users.id
+            ORDER BY "visitCount" DESC
+            LIMIT 10
+        `)).rows;
+
+		return res.status(200).send(dataBaseRank);
+	} catch (error) {
+		console.log(chalk.bold.red(error));
+		return res.sendStatus(500);
+	}
+}
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
-import {getUser} from '../controllers/usersController.js';
+import {getUser, getUsersRank} from '../controllers/usersController.js';
 import validateToken from '../middlewares/tokenValidation.js';
 
 
 const usersRouter = Router();
 
 usersRouter.get('/users/:id', validateToken, getUser);
+usersRouter.get('/ranking', getUsersRank);
+
 export default usersRouter;
