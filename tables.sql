e079ea803e5b896031216df74d64c678b772f33c
Matheus Moura <matheuslnmoura@gmail.com>
2022-06-13 10:35:08 -0300

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -32,7 +32,7 @@ CREATE TABLE "deletedUrls"(
     "shortUrl" TEXT NOT NULL,
     "url" TEXT NOT NULL,
     "userId" INTEGER NOT NULL REFERENCES "users"("id"),
-    "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    "deletedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     "visitCount" INTEGER NOT NULL DEFAULT 0
 )
 
